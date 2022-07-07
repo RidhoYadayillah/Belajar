84b308ff4385fd5e45897d773fe20411fba97110
ridho <ridhoyadayillah@gmail.com>
2022-07-07 22:16:49 +0700

    Buat Function Power Calculation

---
+++ belajar.py
@@ -0,0 +1,7 @@
+def Power_Calculation():
+    Tegangan = input("Nilai Tegangan :")
+    Arus = input ("Nilai Arus :")
+    Power = int(Tegangan) * int(Arus)
+
+    print(str(Power))
+Power_Calculation()
